90036df131601cce8c2b58a8f099181257bfc7b7
nrifath2009 <nrifath2009@gmail.com>
2018-01-23 17:20:32 +0600

    Implement Lambda Function Example

--- LambdaFunctionExample/Program.cs
+++ LambdaFunctionExample/Program.cs
@@ -8,8 +8,29 @@ namespace LambdaFunctionExample
 {
     class Program
     {
+        public delegate int MyDelegate(int x);
+        public delegate void MyDelegate2(int x, string prefix);
+        public delegate bool ExprDelegate(int x);
         static void Main(string[] args)
         {
+            MyDelegate foo = (x) => x * x;
+            Console.WriteLine("The result of foo is {0}",foo(5));
+            foo = (x) => x * 10;
+            Console.WriteLine("The result of bar is {0}", foo(5));
+
+
+            MyDelegate2 bar = (x, y) =>
+            {
+                Console.WriteLine("The two arg lambda: {1} {0}", x * 10, y);
+            };
+            bar(25, "some string");
+
+            ExprDelegate baz = (x) => x > 10;
+            Console.WriteLine("Calling baz with 5 {0}", baz(5));
+            Console.WriteLine("Calling baz with 15 {0}", baz(15));
+
+
+            Console.ReadKey();
         }
     }
 }
